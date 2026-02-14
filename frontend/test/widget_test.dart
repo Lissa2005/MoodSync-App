266051a530e1e133092e0b99e60eb01e86c4cd44
ChethumiGeorge <onelageg05@gmail.com>
2026-02-14 17:22:43 +0530

    removed unused import

--- frontend/test/widget_test.dart
+++ frontend/test/widget_test.dart
@@ -4,11 +4,7 @@
 // utility in the flutter_test package. For example, you can send tap and scroll
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
-import 'package:flutter/cupertino.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:moodsync/main.dart';
-import 'package:moodsync/screens/start_up_screens/welcome_screen.dart';
 
 void main() {
   testWidgets('App launches without crashing', (WidgetTester tester) async {
