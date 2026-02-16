0148fae7384dccbcb1680f1974ca1576fc44bc53
Methuni Dulathma Wickramage <m.dulathma24@gmail.com>
2026-02-16 15:02:29 +0530

    refined user.py and session.py for database connection

--- backend/app/db/session.py
+++ backend/app/db/session.py
@@ -3,5 +3,5 @@ from sqlalchemy.orm import sessionmaker
 
 DATABASE_URL = "postgresql://postgres:password@localhost:5432/moodsync"
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(settings.DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
