1c977f585277d5417a174808f163cef23f1b2535
Lissa2005 <mellissamangalaari2@gmail.com>
2026-03-13 14:34:01 +0530

    Updated the Database connection
    Moved the the database to a cloud database Neon

--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -1,1 +1,22 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
 
+# Load environment variables from .env
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+# Create database engine
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True  # helps prevent dropped connections in cloud DBs
+)
+
+# Create session factory
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
