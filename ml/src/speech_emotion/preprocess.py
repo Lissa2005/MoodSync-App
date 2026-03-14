cd73e9376343f7d5264e646ce0f3024e34b78b47
Gimantha Marasinghe <gimantha.webdev@gmail.com>
2026-03-14 19:04:10 +0530

    Improve audio processing

--- ml/src/speech_emotion/preprocess.py
+++ ml/src/speech_emotion/preprocess.py
@@ -1,5 +1,4 @@
 import librosa
-import numpy as np
 
 
 def load_audio(path):
@@ -7,10 +6,3 @@ def load_audio(path):
     speech, sr = librosa.load(path, sr=16000)
 
     return speech
-
-
-def extract_features(path):
-
-    audio = load_audio(path)
-
-    return audio
