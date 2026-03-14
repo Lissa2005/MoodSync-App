ae4fd309fd0731245adb893d4c11fa6038ac3c73
Gimantha Marasinghe <gimantha.webdev@gmail.com>
2026-03-14 13:25:28 +0530

    Created text inference

---
+++ ml/inference/text_inference.py
@@ -0,0 +1,46 @@
+import torch
+from transformers import DistilBertTokenizer
+from transformers import DistilBertForSequenceClassification
+
+from src.common.emotion_mapper import map_emotion
+
+
+model_path = "ml/models/text_emotion_model"
+
+tokenizer = DistilBertTokenizer.from_pretrained(model_path)
+
+model = DistilBertForSequenceClassification.from_pretrained(model_path)
+
+model.eval()
+
+
+labels = [
+    "sadness",
+    "joy",
+    "love",
+    "anger",
+    "fear",
+    "surprise"
+]
+
+
+def predict_text_emotion(text):
+
+    inputs = tokenizer(
+        text,
+        return_tensors="pt",
+        truncation=True,
+        padding=True
+    )
+
+    with torch.no_grad():
+
+        outputs = model(**inputs)
+
+    predicted_class = torch.argmax(outputs.logits).item()
+
+    emotion = labels[predicted_class]
+
+    mapped_emotion = map_emotion(emotion)
+
+    return mapped_emotion
