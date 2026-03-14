9cdb88a21c2c904544fb6165271aa0753568598c
Gimantha Marasinghe <gimantha.webdev@gmail.com>
2026-03-14 11:34:42 +0530

    Create text model train code

---
+++ ml/src/text_emotion/train.py
@@ -0,0 +1,57 @@
+import torch
+from transformers import Trainer, TrainingArguments
+
+from src.text_emotion.dataset import load_text_datasets
+from src.text_emotion.model import load_text_model
+from src.text_emotion.tokenizer import load_tokenizer
+
+
+def tokenize_function(examples, tokenizer):
+
+    return tokenizer(
+        examples["text"],
+        padding="max_length",
+        truncation=True,
+        max_length=128
+    )
+
+
+def train():
+
+    dataset = load_text_datasets()
+
+    tokenizer = load_tokenizer()
+
+    tokenized_dataset = dataset.map(
+        lambda x: tokenize_function(x, tokenizer),
+        batched=True
+    )
+
+    model = load_text_model(num_labels=6)
+
+    training_args = TrainingArguments(
+        output_dir="../../models/text_model",
+        evaluation_strategy="epoch",
+        learning_rate=2e-5,
+        per_device_train_batch_size=16,
+        per_device_eval_batch_size=16,
+        num_train_epochs=3,
+        weight_decay=0.01,
+        logging_dir="../../logs",
+    )
+
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        train_dataset=tokenized_dataset["train"],
+        eval_dataset=tokenized_dataset["validation"],
+        tokenizer=tokenizer,
+    )
+
+    trainer.train()
+
+    trainer.save_model("../../models/text_emotion_model")
+
+
+if __name__ == "__main__":
+    train()
