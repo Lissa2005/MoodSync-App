7d5242da6b39eb0ea05ad70d263f058606840231
Lissa2005 <mellissamangalaari2@gmail.com>
2026-03-15 16:40:09 +0530

    Fixed Syntax Error

--- backend/app/routes/moods.py
+++ backend/app/routes/moods.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from app.db.session import get_db
-from app.db.model.moods import Mood
+from app.model.moods import Mood
 
 router = APIRouter()
 
